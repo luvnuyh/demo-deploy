4c2f1bbe49cefcea3974fb62d7ed6cc8f75e3c7d
luvnuyh <sohyun5429@gmail.com>
2025-01-17 17:25:49 +0900

    Feat: gitaction application.properties 동적 생성을 통한 보안 정보 관리

--- src/main/java/org/example/demodeploy/DemoDeployApplication.java
+++ src/main/java/org/example/demodeploy/DemoDeployApplication.java
@@ -5,13 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoDeployApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(DemoDeployApplication.class, args);
     }
 }
 
-
-    // 단위 테스트
-    // MVC 테스트 -> 목업통해서 가상 처리 가능
-    // RestAPI 테스트 가능
 
