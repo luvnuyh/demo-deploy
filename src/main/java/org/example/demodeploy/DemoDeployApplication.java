769da22ff479334707c27151fedaed1e544babbd
luvnuyh <sohyun5429@gmail.com>
2025-01-17 12:19:18 +0900

    Feat: gitaction 빌드 작업 추가, EC2에 업로드!

--- src/main/java/org/example/demodeploy/DemoDeployApplication.java
+++ src/main/java/org/example/demodeploy/DemoDeployApplication.java
@@ -5,12 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoDeployApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDeployApplication.class, args);
+    }
+}
 
-   void contextLoads() {
-       System.out.println("단위 테스트 호출!");
-   }
 
     // 단위 테스트
     // MVC 테스트 -> 목업통해서 가상 처리 가능
     // RestAPI 테스트 가능
-}
+
