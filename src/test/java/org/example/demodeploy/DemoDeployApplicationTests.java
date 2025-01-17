dff8d187abe2f64bdcb3b83314326027dcb4cdd5
luvnuyh <sohyun5429@gmail.com>
2025-01-17 15:51:25 +0900

    HotFix: 단위 테스트 오류 해결

--- src/test/java/org/example/demodeploy/DemoDeployApplicationTests.java
+++ src/test/java/org/example/demodeploy/DemoDeployApplicationTests.java
@@ -1,4 +1,4 @@
-package org.example.demodeploy;
+package com.example.demodeploy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +8,15 @@ class DemoDeployApplicationTests {
 
     @Test
     void contextLoads() {
+        // 단위 테스트
+        // 에러 강제 발생 시킴 -> 중단여부 체크
+        // 테스트 실패되면 반영 X
+        // int a = 1/0;
+        // throw new RuntimeException("오류발생");
+        // MVC 테스트 -> 목업통해서 가상 처리 가능
+        // RestAPI 테스트 가능
+        // 메세지가 보지이는 않지만,  차후 코드로 확인
+        //System.out.println("단위 테스트 호출!! echo");
     }
 
 }
